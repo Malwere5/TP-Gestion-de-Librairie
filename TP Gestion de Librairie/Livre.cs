689f1016fd35988a971a01cde6667b88f50694f0
victo <victo@MSI>
2024-09-24 19:24:30 +0200

    Ajoutez des fichiers projet.

---
+++ TP Gestion de Librairie/Livre.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Gestion_de_Librairie
+{
+    internal class Livre
+    {
+    }
+}
---
+++ TP Gestion de Librairie/Media.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TP_Gestion_de_Librairie
+{
+    internal class Media
+    {
+        public string Titre { get; set; }
+        public int NumeroDeReference { get; set; }
+        public int NombreDExemplairesDisponibles { get; set; }
+
+        public virtual void AfficherInfos()
+        {
+            Console.WriteLine($"Titre: {Titre}, Numéro de Référence: {NumeroDeReference}, Nombre d'Exemplaires Disponibles: {NombreDExemplairesDisponibles}");
+        }
+    }
+
+    internal class Livre : Media
+    {
+        public string Auteur { get; set; }
+
+        public override void AfficherInfos()
+        {
+            base.AfficherInfos();
+            Console.WriteLine($"Auteur: {Auteur}");
+        }
+    }
+
+    internal class DVD : Media
+    {
+        public int Duree { get; set; }
+
+        public override void AfficherInfos()
+        {
+            base.AfficherInfos();
+            Console.WriteLine($"Durée: {Duree} minutes");
+        }
+    }
+
+    internal class CD : Media
+    {
+        public string Artiste { get; set; }
+
+        public override void AfficherInfos()
+        {
+            base.AfficherInfos();
+            Console.WriteLine($"Artiste: {Artiste}");
+        }
+    }
+}
